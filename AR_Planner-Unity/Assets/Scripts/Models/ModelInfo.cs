ef92d729c9c2f3acbbba2965798d6a0429c361c1
Alicia Pose <66890913+AliciaPoseDiezdelaLastra@users.noreply.github.com>
2023-01-20 04:22:32 -0500

    Update script header

--- AR_Planner-Unity/Assets/Scripts/Models/ModelInfo.cs
+++ AR_Planner-Unity/Assets/Scripts/Models/ModelInfo.cs
@@ -1,8 +1,8 @@
-// This code was developed by Alicia Pose, from Universidad Carlos III de Madrid
-// This script defines some fields of interest that will be associated to every model that will be transferred to 3D Slicer.
-// For instance, it includes information on the model number, name in the local storage and dimensions. 
-// The spine and image models also belong to this class. The fields that don't apply to them have to 
-// be also filled (i.e. "0"), but 3D Slicer won't read them
+// This code was developed by Alicia Pose Díez de la Lastra, from Universidad Carlos III de Madrid
+// This script defines some fields of interest that will be associated to every model transferred to 3D Slicer.
+// For the screws, it includes information on the model number, name in the local storage and dimensions. 
+// The spine and image models also belong to this class. The fields that don't apply to them (such like dimensions) 
+// have to be filled too (i.e. "0"), but 3D Slicer won't read them.
 
 using System.Collections;
 using System.Collections.Generic;
